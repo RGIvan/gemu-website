9038c37305dcd60a2f79a01fe7156f96bc6cf3d3
TuNombre <ivanroalesgarcia@gmail.com>
2025-12-07 21:40:13 +0100

    Cambios en el microservicio de facturas

--- backend-gemu/microservicios/microservicio-facturas/src/main/java/com/tienda/facturas/controllers/FacturaController.java
+++ backend-gemu/microservicios/microservicio-facturas/src/main/java/com/tienda/facturas/controllers/FacturaController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/facturas")
+@RequestMapping("/api/facturas")
 public class FacturaController {
 
     private final FacturaServicio facturaServicio;
