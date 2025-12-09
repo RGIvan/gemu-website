2b045e63b17227e8b2a5634834381a978361adf7
TuNombre <ivanroalesgarcia@gmail.com>
2025-12-09 17:09:23 +0100

    Cambios en el frontend

--- frontend-gemu/src/components/common/FilterPlatform.tsx
+++ frontend-gemu/src/components/common/FilterPlatform.tsx
@@ -6,9 +6,9 @@ import { useState } from "react";
 const platforms = [
   { value: "", label: "Todas" },
   { value: "PlayStation", label: "PlayStation" },
-  { value: "Xbox", label: "Xbox" },
-  { value: "Nintendo", label: "Nintendo" },
-  { value: "PC", label: "PC" },
+  { value: "PlayStation", label: "PlayStation 2" },
+  { value: "Xbox", label: "Xbox 360" },
+  { value: "Nintendo", label: "Nintendo Switch" },
 ];
 
 const FilterPlatform = () => {
