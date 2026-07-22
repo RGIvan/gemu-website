29df77aa0262f4696ea6572b3f3521e870a5ba9a
TuNombre <ivanroalesgarcia@gmail.com>
2026-07-22 14:44:21 +0200

    Cambios en la variable params por Promise por Next.js 16.2

--- frontend-gemu/src/app/api/user/[id]/route.ts
+++ frontend-gemu/src/app/api/user/[id]/route.ts
@@ -3,17 +3,19 @@ import { prisma } from "@/libs/prisma";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> },
 ) {
   try {
+    const { id } = await params;
+
     const user = await prisma.usuarios.findUnique({
-      where: { id: BigInt(params.id) },
+      where: { id: BigInt(id) },
     });
 
     if (!user) {
       return NextResponse.json(
         { error: "Usuario no encontrado" },
-        { status: 404 }
+        { status: 404 },
       );
     }
 
@@ -30,7 +32,7 @@ export async function GET(
     console.error("Error fetching user:", error);
     return NextResponse.json(
       { error: "Error al obtener usuario" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
